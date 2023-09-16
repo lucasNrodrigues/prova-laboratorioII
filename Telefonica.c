ee9fb1868ae9080abd204d248c0b346cd587ecb0
lucas rodrigues <145014506+lucasNrodrigues@users.noreply.github.com>
2023-09-16 14:51:21 -0300

    Update Telefonica.c

--- Telefonica.c
+++ Telefonica.c
@@ -4,13 +4,16 @@
 #include <stdbool.h>
 #include <ctype.h>
 #include <locale.h>
+#include <time.h>
 
 
-typedef struct {
+typedef struct Contato {
     char nome[50];
     char email[50];
     char telefone[15];
+    struct Contato* proximo;
 } Contato;
+
 
 
 #define MAX_CONTATOS 100
@@ -74,11 +77,15 @@ bool validarTelefone(char* telefone) {
     return digitCount >= 8 && digitCount <= 12 && dashCount <= 1;
 }
 
-
 void adicionarContato() {
     Contato novoContato;
+
+    // Exibe o menu para adicionar um contato
+    printf("** Adicionar contato **\n\n");
     printf("Digite o nome: ");
     scanf("%s", novoContato.nome);
+
+    // Valida o nome do contato
     if (!validarNome(novoContato.nome)) {
         printf("Nome inválido.\n");
         return;
@@ -86,6 +93,8 @@ void adicionarContato() {
 
     printf("Digite o e-mail: ");
     scanf("%s", novoContato.email);
+
+    // Valida o e-mail do contato
     if (!validarEmail(novoContato.email)) {
         printf("E-mail inválido.\n");
         return;
@@ -93,76 +102,98 @@ void adicionarContato() {
 
     printf("Digite o telefone: ");
     scanf("%s", novoContato.telefone);
+
+    // Valida o telefone do contato
     if (!validarTelefone(novoContato.telefone)) {
         printf("Telefone inválido.\n");
         return;
     }
 
+    // Calcula o índice do hash do contato
     int indice = hash(novoContato.nome);
 
-    if (strlen(tabela[indice].nome) == 0) {
-        tabela[indice] = novoContato;
+    // Insere o contato na tabela
+    if (tabela[indice].proximo == NULL) {
+        tabela[indice].proximo = (Contato*)malloc(sizeof(Contato));
+        if (tabela[indice].proximo == NULL) {
+            printf("Erro ao alocar memória para o novo contato.\n");
+            return;
+        }
+        *tabela[indice].proximo = novoContato;
+        tabela[indice].proximo->proximo = NULL;
         printf("Contato adicionado com sucesso!\n");
     } else {
-       
-        Contato* atual = &tabela[indice];
+        Contato* atual = tabela[indice].proximo;
         while (atual->proximo != NULL) {
             atual = atual->proximo;
         }
-        atual->proximo = (Contato*)malloc(sizeof(Contato)); 
+        atual->proximo = (Contato*)malloc(sizeof(Contato));
         if (atual->proximo == NULL) {
             printf("Erro ao alocar memória para o novo contato.\n");
             return;
         }
-        atual = atual->proximo;
-        *atual = novoContato;
-        atual->proximo = NULL;
+        *atual->proximo = novoContato;
+        atual->proximo->proximo = NULL;
         printf("Contato adicionado com sucesso!\n");
     }
 }
 
 void listarContatos() {
     int i;
+    int contatosListados = 0;  // Variável para controlar o número de contatos listados
 
     printf("Lista de contatos:\n");
     printf("Nome | E-mail | Telefone\n");
 
     for (i = 0; i < MAX_CONTATOS; i++) {
-        if (strlen(tabela[i].nome) > 0) {
-            Contato* atual = &tabela[i];
+        if (tabela[i].proximo != NULL) {
+            Contato* atual = tabela[i].proximo;
             while (atual != NULL) {
                 printf("%s | %s | %s\n", atual->nome, atual->email, atual->telefone);
                 atual = atual->proximo;
+                contatosListados++;
+
+                // Adiciona uma pausa após listar cada grupo de contatos
+                if (contatosListados % 10 == 0) {
+                    printf("Pressione Enter para continuar...");
+                    while (getchar() != '\n');
+                }
             }
         }
     }
+
+    printf("\n");
 }
 
 void buscarContato() {
     char nome[50];
     int indice;
 
-    
+    // Exibe o menu para buscar um contato
+    printf("** Buscar contato **\n\n");
     printf("Digite o nome do contato a ser buscado: ");
     scanf("%s", nome);
 
-    
+    // Calcula o índice do hash do contato
     indice = hash(nome);
 
-    if (strlen(tabela[indice].nome) > 0) {
-       
+    // Busca o contato na tabela
+    Contato* contato = tabela[indice].proximo;
+    while (contato != NULL && strcmp(contato->nome, nome) != 0) {
+        contato = contato->proximo;
+    }
+
+    // Imprime o contato encontrado
+    if (contato != NULL) {
         printf("Contato encontrado:\n");
-        printf("Nome: %s\n", tabela[indice].nome);
-        printf("E-mail: %s\n", tabela[indice].email);
-        printf("Telefone: %s\n", tabela[indice].telefone);
+        printf("Nome: %s\n", contato->nome);
+        printf("E-mail: %s\n", contato->email);
+        printf("Telefone: %s\n", contato->telefone);
     } else {
-       
         printf("Contato não encontrado.\n");
     }
 }
-
 void exportarContatos() {
-    
     FILE* arquivo = fopen("contatos.txt", "w");
 
     if (arquivo == NULL) {
@@ -172,36 +203,41 @@ void exportarContatos() {
 
     fprintf(arquivo, "nome,email,telefone\n");
 
-   
-    int i;
-    for ( i = 0; i < MAX_CONTATOS; i++) {
-        if (strlen(tabela[i].nome) > 0) {
-            fprintf(arquivo, "%s,%s,%s\n", tabela[i].nome, tabela[i].email, tabela[i].telefone);
+    for (int i = 0; i < MAX_CONTATOS; i++) {
+        if (tabela[i].proximo != NULL) {
+            Contato* atual = tabela[i].proximo;
+            while (atual != NULL) {
+                fprintf(arquivo, "%s,%s,%s\n", atual->nome, atual->email, atual->telefone);
+                atual = atual->proximo;
+            }
         }
     }
 
-    
     fclose(arquivo);
 
-    
     printf("Contatos exportados com sucesso.\n");
 }
 
 int main() {
     int opcao;
-    setlocale(LC_ALL, "Portuguese");
+    int tentativas = 0;
 
-   
-    while (1) {
-        printf(" MENU:\n");
+    // Exibe o menu principal
+    while (true) {
+        printf("** Agenda de contatos **\n\n");
         printf("1. Adicionar contato\n");
         printf("2. Listar contatos\n");
         printf("3. Buscar contato\n");
         printf("4. Exportar contatos\n");
-        printf("5. Sair\n");
-        printf("Digite a opcao desejada: \n");
-        scanf("%d", &opcao);
+        printf("5. Sair\n\n");
 
+        // Valida a opção selecionada
+        do {
+            printf("Digite a opção desejada: ");
+            while (getchar() != '\n');  // Limpar o buffer de entrada
+        } while (scanf("%d", &opcao) != 1 || opcao < 1 || opcao > 5);
+
+        // Processa a opção selecionada
         switch (opcao) {
             case 1:
                 adicionarContato();
@@ -217,12 +253,15 @@ int main() {
                 break;
             case 5:
                 printf("Saindo...\n");
-                exit(0);
-            default:
-                printf("Opcaoo invalida. Tente novamente.\n");
+                return 0;
         }
 
-        printf("\n");
+        // Verifica se o usuário digitou uma opção válida
+        if (tentativas == 3) {
+            printf("Excedeu o número máximo de tentativas.\n");
+            return 1;
+        }
     }
-}
 
+    return 0;
+}
